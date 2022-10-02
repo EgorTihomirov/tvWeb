adaeac222e5a23978e660fba6490ddde71ab7cdc
Egor Tihomirov <tegor8201@gmail.com>
2022-10-02 11:02:31 +0300

    TV

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>Телевизор</h1>
+  <div class="square"></div>
+  <div class="rectangle"></div>
+  <div class="sliderVolume"></div>
+  <div class="sliderLight"></div>
 </template>
 
 <script>
@@ -20,4 +24,39 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.square {
+  position: relative;
+  left: 730px;
+  top: 60px;
+	width: 300px;
+	height: 300px;
+	background: rgb(46, 46, 46);
+}
+.rectangle {
+  position: relative;
+  left: 1400px;
+	width: 150px;
+	height: 360px;
+	background: rgb(32, 27, 27);
+}
+
+.sliderVolume {
+  position: relative;
+  bottom: 285px;
+  left: 730px;
+  width: 300px;
+  height: 50px;
+  background: rgb(156, 156, 156);
+}
+
+.sliderLight {
+  position: relative;
+  left: 730px;
+  bottom: 275px;
+  width: 300px;
+  height: 50px;
+  background: rgb(156, 156, 156);
+}
+
 </style>
